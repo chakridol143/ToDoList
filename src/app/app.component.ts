028f3c65a02a85d042ff9552db652a7887efaa05
Chakri Chikkaala <chakridol143@gmail.com>
2025-03-16 23:03:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo';
+  taskText:string=''
+  completedTask:string[]=[]
+  tasks:string[]=[]
+  AddTask(){
+    let i:any = document.getElementById("in")
+    if(this.taskText==''){
+      window.alert("Your input task is empty..");
+    }
+    else{
+      this.tasks.push(this.taskText);
+      this.taskText=''
+    }
+    
+      i.value=null
+  }
+  AddText(event:any){
+   this.taskText= (event.target.value)
+  }
+  delete(index:any){
+    const audio= new Audio("assets/Delete.m4a")
+    audio.play();
+    this.tasks.splice(index,1)
+  }
+  CompletedTask(index:any){
+    const audio= new Audio("assets/Completed.m4a")
+    audio.play();
+    this.completedTask.push(this.tasks[index])
+    this.tasks.splice(index,1)
+  }
 }
